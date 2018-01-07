3767cd356f6d1e5d5078a451105d2a04e91419ea
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-07 16:35:16 -0500

    Remove the check for the at rule at the start of execution (for now)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import postcss from 'postcss';
-import stylelint from 'stylelint';
+import postcss from "postcss";
+import stylelint from "stylelint";
 
-import optsDefaults from '../optsDefaults';
-import { specialClasses } from './specialClasses';
-import { makeSidesClasses } from './makeSidesClasses';
-import { makeAllSidesClasses } from './makeAllSidesClasses';
-import { makeAxisClasses } from './makeAxisClasses';
-import { validateOpts } from './validateOpts';
+import optsDefaults from "../optsDefaults";
+import { specialClasses } from "./specialClasses";
+import { makeSidesClasses } from "./makeSidesClasses";
+import { makeAllSidesClasses } from "./makeAllSidesClasses";
+import { makeAxisClasses } from "./makeAxisClasses";
+import { validateOpts } from "./validateOpts";
 
 const makeRules = opts =>
     new Promise(async resolve => {
         const code = `${specialClasses}
-${makeAllSidesClasses(opts, 'margin')}
-${makeSidesClasses(opts, 'margin')}
-${makeAxisClasses(opts, 'margin')}
-${makeAllSidesClasses(opts, 'padding')}
-${makeSidesClasses(opts, 'padding')}
-${makeAxisClasses(opts, 'padding')}
+${makeAllSidesClasses(opts, "margin")}
+${makeSidesClasses(opts, "margin")}
+${makeAxisClasses(opts, "margin")}
+${makeAllSidesClasses(opts, "padding")}
+${makeSidesClasses(opts, "padding")}
+${makeAxisClasses(opts, "padding")}
 `;
 
         const lintResult = await stylelint.lint({ code, fix: true });
@@ -25,13 +25,7 @@ ${makeAxisClasses(opts, 'padding')}
 
 export const spacingUtils = (opts = optsDefaults) => (root, result) =>
     new Promise((resolve, reject) => {
-        if (root.source.input.css.indexOf('@spacing-utils') === -1) {
-            result.warn('@spacing-utils at rule not found');
-            reject('@spacing-utils at rule not found');
-            return;
-        }
-
-        root.walkAtRules('spacing-utils', async rule => {
+        root.walkAtRules("spacing-utils", async rule => {
             try {
                 const validatedOpts = await validateOpts(opts);
                 const css = await makeRules(validatedOpts);
@@ -44,6 +38,6 @@ export const spacingUtils = (opts = optsDefaults) => (root, result) =>
         });
     });
 
-const plugin = postcss.plugin('spacing-utils', spacingUtils);
+const plugin = postcss.plugin("spacing-utils", spacingUtils);
 
 export default plugin;
